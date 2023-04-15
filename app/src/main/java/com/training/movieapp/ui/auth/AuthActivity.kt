339697ef5c39097451bc762babe7f86cbe528fda
congdanh2504 <danhuchiha2002@gmail.com>
2023-04-15 09:24:13 +0700

    Check authentication in splash screen

--- app/src/main/java/com/training/movieapp/ui/auth/AuthActivity.kt
+++ app/src/main/java/com/training/movieapp/ui/auth/AuthActivity.kt
@@ -1,20 +1,37 @@
 package com.training.movieapp.ui.auth
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import androidx.activity.viewModels
 import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
+import com.google.firebase.auth.FirebaseAuth
 import com.training.movieapp.R
 import com.training.movieapp.ui.auth.viewmodel.AuthViewModel
+import com.training.movieapp.ui.main.MainActivity
 import dagger.hilt.android.AndroidEntryPoint
+import javax.inject.Inject
 
 @AndroidEntryPoint
 class AuthActivity : AppCompatActivity() {
     private val authViewModel: AuthViewModel by viewModels()
+    @Inject lateinit var firebaseAuth: FirebaseAuth
 
     override fun onCreate(savedInstanceState: Bundle?) {
-        installSplashScreen()
+        val splashScreen = installSplashScreen()
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_auth)
+//        firebaseAuth.signOut()
+        splashScreen.setKeepOnScreenCondition {
+            true
+        }
+        if (firebaseAuth.currentUser == null) {
+            splashScreen.setKeepOnScreenCondition {
+                false
+            }
+        } else {
+            startActivity(Intent(this, MainActivity::class.java))
+            finish()
+        }
     }
 }
